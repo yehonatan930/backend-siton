231ebd2564505b526069be1dbf5a5e836eb11b57
hackathon IDF <hack@cs.colman.ac.il>
2021-06-06 17:39:46 +0300

    added activities page

---
+++ siton-frontend/src/views/ActivitiesPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="d-flex justify-center">
+      <Activity> </Activity>
+      </div>
+  </div>
+</template>
+
+<script>
+import Activity from "../components/Activity";
+
+export default {
+  components: {
+    Activity
+  }
+};
+</script>
+
+<style></style>
