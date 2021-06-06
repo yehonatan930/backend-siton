04f2cddc203f83e7869e9f6f79626bde52add38d
yehonatan930 <yehonatan930@gmail.com>
2021-06-06 17:44:02 +0300

    feat: navber and vuetify

---
+++ siton-frontend/src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-navigation-drawer absolute permanent right color="#040848" dark>
+    <v-list-item two-line>
+      <v-list-item-content class="text-center">
+        <v-list-item-avatar height="100" width="100">
+          <img
+            id="user-img"
+            src="https://randomuser.me/api/portraits/women/81.jpg"
+          />
+        </v-list-item-avatar>
+        <v-list-item-title class="font-weight-bold">סיטון</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-list dense>
+      <v-list-item-group mandatory>
+        <v-list-item
+          v-for="option in options"
+          :key="option.title"
+          link
+          :href="option.link"
+        >
+          <v-list-item-content class="text-right">
+            <v-list-item-title>{{ option.title }}</v-list-item-title>
+          </v-list-item-content>
+          <v-list-item-icon>
+            <v-icon>{{ option.icon }}</v-icon>
+          </v-list-item-icon>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: "navbar",
+
+  data() {
+    return {
+      options: [
+        { title: "לוח פעילויות", icon: "mdi-clipboard-text", link: "" },
+        { title: "מפה", icon: "mdi-map", link: "" },
+        { title: "דיווחים", icon: "mdi-cellphone-nfc", link: "" }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
