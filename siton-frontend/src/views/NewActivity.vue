9d6382197f4893d25ae6e503badc699272e29479
yehonatan930 <yehonatan930@gmail.com>
2021-06-07 14:09:37 +0300

    feat: new activity page

--- siton-frontend/src/views/NewActivity.vue
+++ siton-frontend/src/views/NewActivity.vue
@@ -1,9 +1,168 @@
 <template>
-  <p></p>
+  <div>
+    <v-card width="700" class="mt-5 rounded-xl">
+      <v-form ref="form" v-model="valid" lazy-validation class="ma-4">
+        <v-select
+          v-model="activity.kind"
+          :items="activityKinds"
+          :rules="[v => !!v || 'צריך לבחור פעילות!']"
+          label="סוג הפעולה"
+          required
+        ></v-select>
+
+        <v-menu
+          ref="menu"
+          v-model="menu"
+          :close-on-content-click="false"
+          :return-value.sync="activity.date"
+          transition="scale-transition"
+          offset-y
+          min-width="auto"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="activity.date"
+              label="תאריך מתוכנן לפעילות"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+              required
+              clearable
+            ></v-text-field>
+          </template>
+          <v-date-picker v-model="activity.date" no-title scrollable>
+            <v-spacer></v-spacer>
+            <v-btn text color="primary" @click="menu = false">
+              ביטול
+            </v-btn>
+            <v-btn text color="primary" @click="$refs.menu.save(activity.date)">
+              אישור
+            </v-btn>
+          </v-date-picker>
+        </v-menu>
+        <v-menu
+          ref="menu2"
+          v-model="menu2"
+          :close-on-content-click="false"
+          :nudge-right="40"
+          :return-value.sync="activity.time"
+          transition="scale-transition"
+          offset-y
+          max-width="290px"
+          min-width="290px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="activity.time"
+              label="זמן מתוכנן לפעילות"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+              required
+              clearable
+            ></v-text-field>
+          </template>
+          <v-time-picker
+            v-model="activity.time"
+            full-width
+            @click:minute="$refs.menu2.save(activity.time)"
+            scrollable
+          ></v-time-picker>
+        </v-menu>
+
+        <v-text-field
+          v-model="activity.plannedForce"
+          :rules="[v => !!v || 'צריך להזין כוח מתוכנן!']"
+          label="כוח מתוכנן"
+          required
+          clearable
+        ></v-text-field>
+
+        <v-text-field
+          v-model="activity.goal"
+          :rules="[v => !!v || 'צריך להזין מטרת פעילות!']"
+          label="מטרת הפעילות"
+          required
+          clearable
+        ></v-text-field>
+
+        <v-text-field
+          v-model="activity.authorizer"
+          :rules="[v => !!v || 'צריך להזין מאשר פעילות!']"
+          label="מאשר הפעילות"
+          required
+          clearable
+        ></v-text-field>
+
+        <v-text-field
+          v-model="activity.loaction"
+          :rules="[
+            v => !!v || 'צריך להזין מיקום!',
+            v => /\d+,\d+/.test(v) || 'צריך להזין מיקום תקני!'
+          ]"
+          label="מיקום"
+          required
+          clearable
+        ></v-text-field>
+        <v-flex class="text-left">
+          <v-btn
+            class="mb-2 white--text"
+            type="submit"
+            :disabled="!valid"
+            color="#3e4174"
+          >
+            הוסף פעילות
+          </v-btn>
+
+          <v-btn
+            @click="clear"
+            color="#3e4174"
+            class="ml-4 mr-4 mb-2 white--text"
+          >
+            נקה
+          </v-btn>
+        </v-flex>
+      </v-form>
+    </v-card>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    activity: {
+      date: null,
+      time: null,
+      plannedForce: "",
+      goal: null,
+      authorizer: null,
+      kind: null,
+      location: null
+    },
+
+    menu: false,
+    menu2: false,
+
+    valid: false,
+
+    activityKinds: ["פטרול", "מארב", "מחסן רכבים"]
+  }),
+
+  methods: {
+    submit() {},
+    clear() {
+      this.activity = {
+        date: null,
+        time: null,
+        plannedForce: "",
+        goal: null,
+        authorizer: null,
+        kind: null,
+        location: null
+      };
+    }
+  }
+};
 </script>
 
-<style></style>
+<style scoped></style>
