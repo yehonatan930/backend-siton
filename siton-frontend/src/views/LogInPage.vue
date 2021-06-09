f9a1a9b08a3d31d9b7230912e47f9d634f037581
yehonatan930 <yehonatan930@gmail.com>
2021-06-09 10:39:40 +0300

    did login not perfect

--- siton-frontend/src/views/LogInPage.vue
+++ siton-frontend/src/views/LogInPage.vue
@@ -14,7 +14,37 @@
             <img class="cops-image" src="../assets/cops.jpg" />
           </v-col>
           <v-col cols="6">
-            <v-btn rounded color="#292b64" @click="sendLogIn()">התחבר</v-btn>
+            <div id="login-container" class="d-flex align-center flex-column">
+              <h1 id="welcome">ברוכים הבאים!</h1>
+              <v-form>
+                <v-text-field
+                  name="login"
+                  label="שם משתמש"
+                  type="text"
+                  required
+                >
+                </v-text-field>
+                <v-text-field
+                  id="password"
+                  name="password"
+                  label="סיסמה"
+                  type="password"
+                  required
+                ></v-text-field>
+                <div class="d-flex align-center">
+                  <v-btn
+                    x-large
+                    id="login-btn"
+                    rounded
+                    color="#292b64"
+                    type="submit"
+                    @click="sendLogIn()"
+                  >
+                    התחבר
+                  </v-btn>
+                </div>
+              </v-form>
+            </div>
           </v-col>
         </v-row>
       </v-container>
@@ -47,5 +77,28 @@ export default {
   height: 100%;
   width: 100%;
   object-fit: cover;
+}
+
+#login-container {
+  padding-top: 10%;
+  line-height: 2.5 !important;
+}
+
+#welcome {
+  font-weight: bold;
+  font-size: 400%;
+}
+
+.v-form {
+  width: 60%;
+  padding-top: 5%;
+}
+
+#login-btn {
+  margin-top: 5%;
+  margin-right: 33%;
+  padding-right: 10%;
+  padding-left: 10%;
+  font-size: 1.2em;
 }
 </style>
